4dc2a4357177b0ac6339de41c75303f29d05a182
Wilfred Almeida <60785452+WilfredAlmeida@users.noreply.github.com>
2025-05-11 11:24:23 +0000

    refactor: test file, add expected output for init
    Signed-off-by: GitHub <noreply@github.com>

--- tests/cli/test.sh
+++ tests/cli/test.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e -u -o pipefail
 
-script_dir=$(dirname "${0}")
+script_dir=$(pwd)
 output_dir="${script_dir}/output"
 
 anchor_cli(){
@@ -17,9 +17,10 @@ setup_test() {
 }
 
 diff_test() {
-  actual_dir="${output_dir}/${1}"
-  expected_dir="${script_dir}/${1}"
-  diff --oneline -r "${actual_dir}" "${expected_dir}"
+  cd "${script_dir}"
+  actual_dir="${output_dir}/${1}/test-program"
+  expected_dir="${script_dir}/expected/${1}/test-program"
+  diff -u -r "${actual_dir}" "${expected_dir}"
 }
 
 # init
